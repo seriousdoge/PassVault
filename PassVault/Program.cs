c9d53c1852995acb2b3c28c3fda4f001095fd678
seriousdoge <67345591+seriousdoge@users.noreply.github.com>
2020-06-28 00:44:51 +0100

    Update Program.cs

--- PassVault/Program.cs
+++ PassVault/Program.cs
@@ -8,9 +8,7 @@ namespace PassVault
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
